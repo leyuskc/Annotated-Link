3ba7d3ec210d1feb684c2905dc83dc6c957450ad
leyuskckiran1510 <kdhakal1510@gmail.com>
2023-01-14 22:38:36 +0545

    selection completed but bugging if any ohrt child is present in the selection

--- ExtensionChrome/background.js
+++ ExtensionChrome/background.js
@@ -1,29 +1,28 @@
-//loading fix datas to storage in the begining
+import {
+    menus
+} from './l.js'
 chrome.runtime.onInstalled.addListener(async() => {
-    chrome.contextMenus.create({
-        id: 'hl',
-        title: "AnotateLink",
-        type: 'normal',
-        contexts: ['selection'],
-    });
+    for (let [id, title] of Object.entries(menus)) {
+        chrome.contextMenus.create({
+            id: id,
+            title: title,
+            type: 'normal',
+            contexts: ['selection'],
+        })
+    }
 })
+
 
 chrome.contextMenus.onClicked.addListener((item, tab) => {
     let id = item.menuItemId
-    let slected = item.selectionText
-    console.log(id, slected)
-});
-
-
-
-
-let background = (run) => {
-    chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-        if (details.url == "chrome://newtab/") {
-            return
-        }
-        if (check(details.url) && details.frameType == "outermost_frame") {
-            console.log(details.url)
-        }
-    })
-}
+    let selection = item.selectionText
+    chrome.scripting.executeScript({
+            target: {
+                tabId: tab.id,
+                allFrames: true
+            },
+            files: ['pop.js'],
+        },
+        () => {})
+    console.log(id, selection, item)
+})
---
+++ ExtensionFireFox/l.js
@@ -0,0 +1,4 @@
+export const menus = {
+    'hl': 'Highlight',
+    'cm': 'Comment'
+};
